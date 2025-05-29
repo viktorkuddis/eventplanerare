97b09631c093b75ed6989c332a7ae49c14cab95f
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-05-29 21:54:42 +0200

    bones för home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
-import { SignOutButton } from "@clerk/clerk-react";
+import { SignOutButton, UserButton } from "@clerk/clerk-react";
 
 const Home = () => {
   return (
     <>
-      Detta är feedet
-      <div>
-        Home DU ÄR INLOGGAD BÄSTA du
-        <br />
-        <SignOutButton />
-      </div>
+      Detta är feedet Home DU ÄR INLOGGAD BÄSTA du
+      <UserButton />
+      <hr />
+      <h2>Pågår just nu(breda kort o sidoskroll)</h2>
+      <br />
+      <h2>Nästa event(lista)</h2>
+      <br />
+      <h2>Dina events (mindre kort sidoscroll)</h2>
+      <br />
+      <h2>Kommande Events (ännu mindre kort? sidoscroll)</h2>
     </>
   );
 };
