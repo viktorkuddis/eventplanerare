f6a0d981de61c89cb716ac9132a66cf585841a61
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-06-04 19:55:19 +0200

    main layout

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -80,36 +80,7 @@ const Home = () => {
 
 
 
-      <div className={`content-container-width-wrapper ${styles.rightNowSection}`}>
-        <h2>Pågår just nu</h2>
-        <div className={styles.cardsContainer}>
-          {
-            // Finns det minst ett aktivt event?
-            context?.ownEvents?.some(e => isEventActive(e.start, e.end))
-              ? (
-                // Ja, rendera kort för alla aktiva event
-                context.ownEvents.map((e, i) =>
-                  isEventActive(e.start, e.end) && (
-                    <EventCard
-                      key={i}
-                      color={e.color}
-                      title={e.title}
-                      start={e.start}
-                      location={e.location}
-                      description={e.description}
-                      layout="landscape"
-                      size="large"
-                    />
-                  )
-                )
-              ) : (
-                // Nej, visa fallback-text
-                <p>Inga aktiva evenemang just nu.</p>
-              )
-          }
 
-        </div>
-      </div>
 
 
       {/* <Carousel items={context?.ownEvents?.map((event, i) =>
@@ -130,47 +101,77 @@ const Home = () => {
       <AddNewEventModal isOpen={showNewEventFormModal} onCloseModal={() => setShowNewEventFormModal(false)} />
 
       <main className="content-container-width-wrapper">
-        <EventCard color={"#FFBF00"}
-          title={"Min event"}
-          start={new Date}
-          location={"hemma hos mig"}
-          description={" lite beskrivning här med och hej och hå. jag hoppas alla vill komma på kalas "} layout={"portrait"} size={"small"} />
+
+        <div className={`${styles.rightNowSection}`}>
+          <h2>Pågår just nu</h2>
+          <div className={styles.cardsContainer}>
+            {
+              // Finns det minst ett aktivt event?
+              context?.ownEvents?.some(e => isEventActive(e.start, e.end))
+                ? (
+                  // Ja, rendera kort för alla aktiva event
+                  context.ownEvents.map((e, i) =>
+                    isEventActive(e.start, e.end) && (
+                      <EventCard
+                        key={i}
+                        color={e.color}
+                        title={e.title}
+                        start={e.start}
+                        location={e.location}
+                        description={e.description}
+                        layout="landscape"
+                        size="large"
+                      />
+                    )
+                  )
+                ) : (
+                  // Nej, visa fallback-text
+                  <p>Inga aktiva evenemang just nu.</p>
+                )
+            }
+          </div>
+        </div>
 
 
-        {context?.ownEvents?.map((event, i) =>
-          <div key={i}><br />
-            <EventCard key={i}
-              color={event.color}
-              title={event.title}
-              start={event.start}
-              location={event.location}
-              description={event.description} layout={"landscape"} size={"large"} /></div>)}
-
-        <br />
-
-        <p>Pågår just nu(breda kort o sidoskroll)</p>
-        <br />
-        <h2>Nästa event(lista)</h2>
-        <br />
-        <h2>Dina events (mindre kort sidoscroll)</h2>
 
 
+        <div>
+          <h2>Nästa event(lista)</h2>
+        </div>
+
+
+        <div>
+          <h2>Dina events (mindre kort sidoscroll)</h2>
+
+          <button className="btn-medium btn-outlined-light-static" onClick={() => {
+
+            setShowNewEventFormModal(true); console.log();
+          }
+          }>
+            + <br />
+            SKAPA <br />
+            EVENT
+          </button>
+          (ska öppna formulär som modal)
+          {/* <AddNewEventForm /> */}
+          <br />
+
+
+        </div>
+
+        <div>
+          <h2>Kommande Events (ännu mindre kort? sidoscroll)</h2>
+
+        </div>
 
 
 
-        <button className="btn-medium btn-outlined-light-static" onClick={() => {
 
-          setShowNewEventFormModal(true); console.log();
-        }
-        }>
-          + <br />
-          SKAPA <br />
-          EVENT
-        </button>
-        (ska öppna formulär som modal)
-        {/* <AddNewEventForm /> */}
-        <br />
-        <h2>Kommande Events (ännu mindre kort? sidoscroll)</h2>
+
+
+
+
+
       </main >
     </div >
   </>
