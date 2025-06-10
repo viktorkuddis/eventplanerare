5fe033e3f03aa6b4ed69be35c74115dbabd2cd04
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-06-10 20:52:49 +0200

    testar padding för header på varje backdrop istl

--- src/pages/HomePage/HomeNotificationsPage/HomeNotificationsPage.tsx
+++ src/pages/HomePage/HomeNotificationsPage/HomeNotificationsPage.tsx
@@ -17,37 +17,7 @@ const HomeNotificationsPage = () => {
                 <hr />
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ut nulla est sit quasi distinctio veniam eligendi ipsam illo adipisci. Ad optio quisquam nam architecto autem! Sunt aperiam delectus similique?
 
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic minima quas, aut provident nulla doloremque nesciunt laudantium harum voluptatum. Minima adipisci incidunt repellendus? Voluptatem quia hic quo dolorum optio. Blanditiis.
-                <hr />
-                <br />
-                <br />
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui numquam, pariatur itaque quos corrupti facilis, placeat atque illum vitae provident amet rem inventore voluptates, quam magni velit quibusdam quae quis.
 
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ut nulla est sit quasi distinctio veniam eligendi ipsam illo adipisci. Ad optio quisquam nam architecto autem! Sunt aperiam delectus similique?
-
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic minima quas, aut provident nulla doloremque nesciunt laudantium harum voluptatum. Minima adipisci incidunt repellendus? Voluptatem quia hic quo dolorum optio. Blanditiis.
-                <hr /> <br />
-                <br />
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui numquam, pariatur itaque quos corrupti facilis, placeat atque illum vitae provident amet rem inventore voluptates, quam magni velit quibusdam quae quis.
-
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ut nulla est sit quasi distinctio veniam eligendi ipsam illo adipisci. Ad optio quisquam nam architecto autem! Sunt aperiam delectus similique?
-
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic minima quas, aut provident nulla doloremque nesciunt laudantium harum voluptatum. Minima adipisci incidunt repellendus? Voluptatem quia hic quo dolorum optio. Blanditiis.
-                <hr /> <br />
-                <br />
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui numquam, pariatur itaque quos corrupti facilis, placeat atque illum vitae provident amet rem inventore voluptates, quam magni velit quibusdam quae quis.
-
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ut nulla est sit quasi distinctio veniam eligendi ipsam illo adipisci. Ad optio quisquam nam architecto autem! Sunt aperiam delectus similique?
-
-                <hr />
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic minima quas, aut provident nulla doloremque nesciunt laudantium harum voluptatum. Minima adipisci incidunt repellendus? Voluptatem quia hic quo dolorum optio. Blanditiis.
-                <hr />
 
             </div>
 
