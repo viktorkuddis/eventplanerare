47f394f981879a2d8f8ca0f732b22273bb1c1dfd
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-06-12 14:43:16 +0200

    bell indicator works

--- src/components/molecules/BellButton/BellButton.tsx
+++ src/components/molecules/BellButton/BellButton.tsx
@@ -3,15 +3,43 @@ import { Bell } from 'react-feather';
 import { useContext } from "react";
 import { AppContext } from "../../../context/AppContext";
 
-
 import styles from "./BellButton.module.css"
 
 
+import { getAppSettingsFromLocalStorage } from "../../../utils/localStorageUtils";
+
+
+//todo:
+// jämför senaste notifikationen och se om dess datum är nyare än då jag öppnade panelen senas. 
+//  om den är nyare så visas plupp och siffra. 
+//  om den är ädre visas ingen plupp och siffra
+// om det inte finns någon datum i localstorage då vsas tom plupp utan siffra.
+
+import { useAuth } from "@clerk/clerk-react";
+
+
 const BellButton = () => {
-
-
     const context = useContext(AppContext)
+    const { userId } = useAuth();
 
+    // hämtar när man senast öppnade
+    let lastOpendDate;
+    if (userId) {
+        lastOpendDate = getAppSettingsFromLocalStorage(userId, "lastNotificationListOpened")
+    }
+    let showNotificationIndicator;
+    if (lastOpendDate && context?.notificationFeed[0]?.date) {
+        const lastDate = new Date(lastOpendDate);
+        console.log("panelen var senast öppen", lastDate)
+
+        const newestNotificationDate = new Date(context.notificationFeed[0].date);
+        console.log("senaste notifikationen:", newestNotificationDate)
+
+        // sätt true om man ligger efter med att titta på notifikationerna
+        showNotificationIndicator = lastDate < newestNotificationDate ? true : false
+    }
+
+    console.log("showNotificationIndicator", showNotificationIndicator)
 
     const navigate = useNavigate();
 
@@ -19,6 +47,12 @@ const BellButton = () => {
     const isNotificationPage = useLocation().pathname
     // console.log(isNotificationPage)
 
+
+    // om lastOpendd date kommer undefined från localstorage betyder det att användaren aldrig klickat där.
+    // då sätter vi en idikator ändå :) så att den klickar där för första gången oavsett om den har notiser eller inte. 
+    // i notiskomponenten sen sätter vi en liten välkomsttext  om de är så att användaren är ny.
+
+    if (!lastOpendDate) showNotificationIndicator = true;
 
     return (
         <div style={{
@@ -42,7 +76,7 @@ const BellButton = () => {
                 <Bell size={"1.5rem"} />
             </button>
 
-            {(context?.notificationFeed?.length || 0) > 0 && (
+            {showNotificationIndicator && (
                 <div style={{
                     backgroundColor: "red",
                     fontSize: "0.75rem",
@@ -57,7 +91,6 @@ const BellButton = () => {
                     top: "5%",
                     left: "55%"
                 }}>
-                    {context?.notificationFeed?.length}
                 </div>
             )}
 
