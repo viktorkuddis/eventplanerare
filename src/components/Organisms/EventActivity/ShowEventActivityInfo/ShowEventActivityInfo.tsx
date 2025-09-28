ef96f0b8088e4546d5affa586be4556e864206e7
Viktor Magnusson <viktor.kuddis@hotmail.com>
2025-09-28 18:20:55 +0200

    korr

--- src/components/Organisms/EventActivity/ShowEventActivityInfo/ShowEventActivityInfo.tsx
+++ src/components/Organisms/EventActivity/ShowEventActivityInfo/ShowEventActivityInfo.tsx
@@ -61,7 +61,7 @@ const ShowEventActivityInfo = ({ item, isOpen, onCloseAction }: PropTypes) => {
                     {new Date(item.startTime).toLocaleDateString("sv-SE", {
                         day: "2-digit",
                         month: "short",
-                    })}, kl. {new Date(item.startTime).toTimeString().slice(0, 5)}
+                    })} kl. {new Date(item.startTime).toTimeString().slice(0, 5)}
                     <br />
                     {item.endTime && (
                         <>
@@ -71,7 +71,7 @@ const ShowEventActivityInfo = ({ item, isOpen, onCloseAction }: PropTypes) => {
                                 new Date(item.endTime).toLocaleDateString("sv-SE", {
                                     day: "2-digit",
                                     month: "short",
-                                }) + ", "}
+                                }) + " "}
                             {/* tiden */}
                             kl. {new Date(item.endTime).toTimeString().slice(0, 5)}
                         </>
